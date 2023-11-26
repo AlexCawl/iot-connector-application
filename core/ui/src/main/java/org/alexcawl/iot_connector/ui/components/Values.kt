2ed01f267c8b2faa5dd3747ec08257651388746f
AlexCawl <babushkinsusu@gmail.com>
2023-11-27 01:57:32 +0500

    fix: some clean-ups in previews

--- core/ui/src/main/java/org/alexcawl/iot_connector/ui/components/Values.kt
+++ core/ui/src/main/java/org/alexcawl/iot_connector/ui/components/Values.kt
@@ -1,5 +1,6 @@
 package org.alexcawl.iot_connector.ui.components
 
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
@@ -10,3 +11,5 @@ val PaddingLarge: Dp = 32.dp
 val IconSmall: Dp = 24.dp
 val IconMedium: Dp = 48.dp
 val IconLarge: Dp = 96.dp
+
+internal fun loremIpsum(words: Int): String = LoremIpsum(words).values.joinToString(" ")
--- core/ui/src/main/java/org/alexcawl/iot_connector/ui/components/dialog/EditActionDialogCaller.kt
+++ core/ui/src/main/java/org/alexcawl/iot_connector/ui/components/dialog/EditActionDialogCaller.kt
@@ -1,8 +1,13 @@
 package org.alexcawl.iot_connector.ui.components.dialog
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
@@ -11,8 +16,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import org.alexcawl.iot_connector.ui.components.loremIpsum
 import org.alexcawl.iot_connector.ui.theme.IoTConnectorTheme
 
 @Composable
@@ -27,7 +35,13 @@ fun EditActionDialogHolder(
     if (isShown) {
         EditActionDialog(title = title,
             content = { text: String, onChange: (String) -> Unit ->
-                TextField(value = text, onValueChange = onChange)
+                TextField(
+                    value = text,
+                    onValueChange = onChange,
+                    textStyle = MaterialTheme.typography.bodyLarge,
+                    minLines = 1,
+                    maxLines = 8
+                )
             },
             onSubmitRequest = onSubmitRequest,
             confirmButton = { text: String, onConfirm: (String) -> Unit ->
@@ -50,22 +64,62 @@ fun EditActionDialogHolder(
 @Preview(name = "Light Theme", uiMode = Configuration.UI_MODE_NIGHT_NO)
 @Preview(name = "Dark Theme", uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
-private fun Preview() {
+private fun StaticPreview() {
+    IoTConnectorTheme {
+        EditActionDialogHolder(
+            isShown = true,
+            title = {
+                Text(
+                    text = loremIpsum(20),
+                    minLines = 1,
+                    maxLines = 1,
+                    style = MaterialTheme.typography.titleLarge,
+                    overflow = TextOverflow.Ellipsis
+                )
+            },
+            onSubmitRequest = {},
+            onDismissRequest = {},
+            initialText = loremIpsum(20)
+        )
+    }
+}
+
+@Preview(name = "Light Theme", uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Preview(name = "Dark Theme", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun DynamicPreview() {
     IoTConnectorTheme {
         var isShown by remember { mutableStateOf(true) }
-        var text by remember { mutableStateOf("hello world!") }
+        var text by remember { mutableStateOf(loremIpsum(500)) }
 
-        Button(onClick = { isShown = isShown.not() }) {
-            Text(text = text)
+        Scaffold {
+            Column(
+                modifier = Modifier.fillMaxSize().padding(it),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Press the button and modify text inside",
+                    maxLines = 1
+                )
+                Button(onClick = { isShown = isShown.not() }) {
+                    Text(
+                        text = text,
+                        maxLines = 1
+                    )
+                }
+            }
         }
 
         EditActionDialogHolder(
             isShown = isShown,
             title = {
                 Text(
-                    text = "Edit username",
+                    text = loremIpsum(20),
+                    minLines = 1,
                     maxLines = 1,
-                    style = MaterialTheme.typography.titleLarge
+                    style = MaterialTheme.typography.titleLarge,
+                    overflow = TextOverflow.Ellipsis
                 )
             },
             onSubmitRequest = {
@@ -77,40 +131,3 @@ private fun Preview() {
         )
     }
 }
-
-/*@Composable
-fun EditActionDialogCaller(
-    onDismissRequest: () -> Unit,
-    expanded: Boolean,
-    title: @Composable () -> Unit,
-    onSubmit: (String) -> Unit,
-    modifier: Modifier = Modifier,
-    initialValue: String = "",
-) {
-    var input: String by remember { mutableStateOf(initialValue) }
-    val save: () -> Unit = {
-        onSubmit(input)
-        onDismissRequest()
-    }
-    val clear: () -> Unit = {
-        input = initialValue
-        onDismissRequest()
-    }
-
-    if (expanded) {
-        AlertDialog(
-            onDismissRequest = clear,
-            title = title,
-            text = {
-                TextField(value = input, onValueChange = { input = it }, keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text))
-            },
-            confirmButton = {
-                TextButton(onClick = save) { Text("Submit") }
-            },
-            dismissButton = {
-                TextButton(onClick = clear) { Text("Cancel") }
-            },
-            modifier = modifier
-        )
-    }
-}*/
